da4404e1187e628ba95e5120818014765487818e
Ernst Vorsteveld <ernst.vorsteveld@gmail.com>
2021-08-15 10:15:01 +0200

    added test for gas

--- parser_test/sternitc.html.parser/UrlHtmlParserTests.cs
+++ parser_test/sternitc.html.parser/UrlHtmlParserTests.cs
@@ -8,19 +8,21 @@ namespace parser_test.sternitc.html.parser
 {
     public class UrlHtmlParserTests
     {
-        [Fact]
-        public void should_load_html_page()
+        [Theory]
+        [InlineData("6412PP", "10", "23", "871688540030933384", "871688540004448586")]
+        [InlineData("6412PP", "10", "27", "871688540008568259", "871688540030933391")]
+        public void should_load_html_page(string postalcode, string number, string segment, string ean1, string ean2)
         {
             var (scheme, domain, url, pagingUrl) = GivenConfiguration();
             var config = BuildConfiguration(url, pagingUrl, domain);
-            var criteria = new EANSearchCriteria("6412PP", "10", "23");
+            var criteria = new EANSearchCriteria(postalcode, number, segment);
 
             var results = new UrlHtmlParser(config).Parse(criteria).ToList();
 
             results.Should().HaveCount(2, "because we expect 2 objects.");
             results.Should().SatisfyRespectively(
-                first => { first.Get("EAN code aansluiting").Should().Be("871688540030933384"); },
-                second => { second.Get("EAN code aansluiting").Should().Be("871688540004448586"); }
+                first => { first.Get("EAN code aansluiting").Should().Be(ean1); },
+                second => { second.Get("EAN code aansluiting").Should().Be(ean2); }
             );
         }
 
